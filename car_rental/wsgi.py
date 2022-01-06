d32f861b8dff03b865682599431d5ce900195c02
Grzegorz Sikorski <grzegorz.sikorski.96@gmail.com>
2022-01-06 18:55:40 +0100

    #19 Change project name

--- car_rental/wsgi.py
+++ car_rental/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pwr_pp project.
+WSGI config for car_rental project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pwr_pp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_rental.settings')
 
 application = get_wsgi_application()
