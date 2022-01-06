de92f35a416c46bcd615ead76d7d0df2b2c4d882
Grzegorz Sikorski <grzegorz.sikorski.96@gmail.com>
2022-01-06 19:07:26 +0100

    #19 Change project name (#20)

--- car_rental/urls.py
+++ car_rental/urls.py
@@ -1,4 +1,4 @@
-"""pwr_pp URL Configuration
+"""car_rental URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
