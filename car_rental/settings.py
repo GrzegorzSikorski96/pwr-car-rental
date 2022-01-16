ac3b2301e602b836dded92d5620ae5369de58b5c
Grzegorz Sikorski <grzegorz.sikorski.96@gmail.com>
2022-01-16 03:51:20 +0100

    #45 Simplify dashboard template

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core.middlewares.login_required_middleware.LoginRequiredMiddleware'
+    # 'core.middlewares.login_required_middleware.LoginRequiredMiddleware'
 ]
 
 ROOT_URLCONF = 'car_rental.urls'
