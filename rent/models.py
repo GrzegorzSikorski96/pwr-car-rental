bc43a47f83994fcfab717b0276a0d4f9c5720a8b
Grzegorz Sikorski <grzegorz.sikorski.96@gmail.com>
2022-03-25 20:21:53 +0100

    Added checking is logged user in get queryset. Code style fix

--- rent/models.py
+++ rent/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.timezone import now
 
+from car.choices.car_status_choices import CarStatus
+
 
 class Rent(models.Model):
     rented_by = models.ForeignKey(
@@ -17,6 +19,14 @@ class Rent(models.Model):
         on_delete=models.DO_NOTHING,
         related_name='rent',
     )
+
+    def save(self, *args, **kwargs) -> None:
+        create = False if self.pk else True
+        super(Rent, self).save(*args, **kwargs)
+
+        if create:
+            self.rented_car.status = CarStatus.RENTED_STATUS
+            self.rented_car.save()
 
 
 class Pricing(models.Model):
