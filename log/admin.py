7f06965fe56c33d5d2627ec5732a5373bd050b3b
Grzegorz Sikorski <grzegorz.sikorski.96@gmail.com>
2022-04-02 15:42:06 +0200

    Displaying messages in car view.

--- log/admin.py
+++ log/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from log.models import ServiceLog, CarLog
+from log.models import ServiceLog, CarLog, MessageLog
 
 admin.site.register(ServiceLog)
 admin.site.register(CarLog)
+admin.site.register(MessageLog)
